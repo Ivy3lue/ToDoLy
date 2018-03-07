e8ffee2c49e488e9332322818b17386b01a94d64
Ivy <gericaleksandra@gmail.com>
2018-03-07 19:45:37 +0100

    Add data class Task and mock data

--- src/com/ivy/MainPresenter.java
+++ src/com/ivy/MainPresenter.java
@@ -1,12 +1,19 @@
 package com.ivy;
 
+import java.util.Date;
+import java.util.List;
+
 /**
  * Contains the implementation of the ToDoLy program in {@link #startProgram()}
  */
 public class MainPresenter extends AbsBasePresenter<Mvp.View> implements Mvp.Presenter {
+
+    private List<Task> tasks;
+
     @Override
     public void onAttach(Mvp.View view) {
         super.onAttach(view);
+        mockData();
         startProgram();
     }
 
@@ -15,5 +22,17 @@ public class MainPresenter extends AbsBasePresenter<Mvp.View> implements Mvp.Pre
             return;
         }
         view.exit();
+    }
+
+    /**
+     * Adds mock data for developing and testing purposes.
+     */
+    private void mockData() {
+        tasks.add(new Task("first mock task", new Date(), false));
+        tasks.add(new Task("second mock task", new Date(), true));
+        tasks.add(new Task("third mock task", null, false));
+        tasks.add(new Task("fourth mock task", new Date(1528236000000L), false));
+        tasks.add(new Task("fifth mock task", new Date(), true));
+        tasks.add(new Task("sixth mock task", new Date(), true));
     }
 }
---
+++ src/com/ivy/Task.java
@@ -0,0 +1,18 @@
+package com.ivy;
+
+import java.util.Date;
+
+/**
+ * Data class for a task.
+ */
+public class Task {
+    public final String name;
+    public final Date dueDate;
+    public final boolean isComplete;
+
+    public Task(String name, Date dueDate, boolean isComplete) {
+        this.name = name;
+        this.dueDate = dueDate;
+        this.isComplete = isComplete;
+    }
+}
